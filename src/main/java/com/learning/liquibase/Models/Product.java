fc55d296f5307c2bcbe985d704e7804992a8ed0d
Tinashe <tinashechasasa7@gmail.com>
2025-07-30 15:45:54 +0200

    added the inserts for liquibase

--- src/main/java/com/learning/liquibase/Models/Product.java
+++ src/main/java/com/learning/liquibase/Models/Product.java
@@ -30,19 +30,5 @@ public class Product {
     @Column
     private Integer price;
 
-    @Override
-    public final boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null) return false;
-        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
-        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
-        if (thisEffectiveClass != oEffectiveClass) return false;
-        Product product = (Product) o;
-        return getId() != null && Objects.equals(getId(), product.getId());
-    }
 
-    @Override
-    public final int hashCode() {
-        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
-    }
 }
